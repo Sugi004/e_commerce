ed948727e31751ca06393d10742ad1b685534501
Sugi004 <sugishivam8@gmail.com>
2025-05-03 22:05:08 -0400

    Edited settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -143,7 +143,7 @@ APPEND_SLASH = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, "staticfiles")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
