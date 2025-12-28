591593e94d1b022cd5d5471abc6d89c8ee1da6fd
Sugi004 <sugishivam8@gmail.com>
2025-12-28 12:19:43 -0500

    Added DB

--- api/products/templatetags/cartTags.py
+++ api/products/templatetags/cartTags.py
@@ -1,6 +1,6 @@
 from django import template
-from ...modules import carts_collection, ObjectId
-
+from ...mongoDb import get_collection
+from bson import ObjectId
 
 register = template.Library()
 
@@ -10,6 +10,7 @@ def get_cart_count(context):
     cart_count = 0
     user_id = request.session.get('user_id')
     if user_id:
+        carts_collection = get_collection("carts")
         cart = carts_collection.find_one({"user_id": ObjectId(user_id)})
         if cart and "items" in cart:
             cart_count = len(cart["items"])
